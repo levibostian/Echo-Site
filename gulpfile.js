07ab118d7dd7135caa8944138c401585ce25cdc5
Levi Bostian <levi.bostian@gmail.com>
2017-01-14 14:03:14 -0600

    Get other pages besides index working.

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,21 @@
 var gulp = require('gulp');
 var uncss = require('gulp-uncss');
+var pug = require('gulp-pug');
 
-gulp.task('default', function() {
-    return gulp.src(['_site/custom.css', 'node_modules/tachyons/css/tachyons.min.css'])
+gulp.task('pug', function buildHTML() {
+    return gulp.src(['_site/**/*.pug'])
+    .pipe(pug({
+        // Your options in here.
+    }))
+});
+
+// Not running this anymore. I am hitting a maximum call stack error with gulp while running this task on tachyons. 
+gulp.task('uncss', function uncss() {
+    return gulp.src(['node_modules/tachyons/css/tachyons.min.css'])
         .pipe(uncss({
             html: ['_site/**/*.html']
         }))
         .pipe(gulp.dest('./_site/dest'));
 });
+
+gulp.task('default', ['pug']);
